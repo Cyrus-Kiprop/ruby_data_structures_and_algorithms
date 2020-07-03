a9088d7ced94049515889fd7adea5ed21f7b86b7
kip <cyruskiprop254@gmail.com>
2020-07-03 19:02:58 +0300

    optimize remove method and add previous_node method

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -10,7 +10,7 @@ end
 class LinkedList
 
   # this class must have two pointer
-
+attr_reader :tail
   @head = nil
   @tail = nil
 
@@ -66,7 +66,34 @@ class LinkedList
     @tail = new_node if (@tail == node)
   end
 
+  def remove(index)
+
+    if index == 0
+     @head = @head.next_node
+   else
+      prev_node = previous_node(index)
+      current = prev_node.next_node
+      if current
+      prev_node.next_node = prev_node.next_node.next_node
+      @tail = prev_node if @tail == current
+      end
+
+    end
+  end
+
+
+
   private
+
+  def previous_node(index)
+    node = @head
+    counter = 0
+    while node && (counter == (index - 2))
+      node = node.next_node
+      counter += 1
+    end
+    node
+  end
 
   def get_node(index)
     node = @head
@@ -87,5 +114,8 @@ list = LinkedList.new
 list.add(3)
 list.add(5)
 list.add_at(0,4)
-
+p list.tail
+p list.print
+p list.remove(2)
+p list.tail.value
 p list.print
