592bd06752d0671206c851132b8cd32686e91098
kip <cyruskiprop254@gmail.com>
2020-07-04 13:05:13 +0300

    stack using linkedlist

---
+++ stacks/stack.rb
@@ -0,0 +1,48 @@
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+class LinkedList
+
+  # this class must have two pointer
+  @head = nil
+  @tail = nil
+
+end
+
+class Stack < LinkedList
+	def push(item)
+		new_node = Node.new(item, @head)
+		@head = new_node
+		@tail = new_node if @tail.nil?
+	end
+
+	def pop
+		raise 'Stack is empty' if @head.nil?
+		value = @head.value
+		@head = @head.next_node
+		value
+	end
+end
+
+stack = Stack.new
+stack.push(3)
+stack.push(5)
+puts stack.pop
+# => 5
+
+stack.push(2)
+stack.push(7)
+puts stack.pop
+# => 7
+
+puts stack.pop
+# => 2
+
+puts stack.pop
+# => 3
